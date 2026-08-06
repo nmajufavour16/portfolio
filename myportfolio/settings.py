2b999e3c574256d2ca331e22f33046857670acdf
nmajufavour16 <nmajufavour16@gmail.com>
2026-08-06 10:52:49 +0100

    added deployment link to allowed hosts

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.vercel.app,127.0.0.1,localhost').split(',')
 
 
 # Application definition
